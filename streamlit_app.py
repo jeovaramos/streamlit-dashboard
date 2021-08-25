d0836822d1625c53628f5765e06b20f50f435269
jeovaramos <silvajunior.jeova@gmail.com>
2021-08-25 10:31:03 -0300

    Code organization and start interactive charts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import folium
-import geopandas as gpd
 import pandas as pd
 import streamlit as st
+import geopandas as gpd
+import plotly.express as px
 from functools import reduce
+import datetime as dt
 from streamlit_folium import folium_static
 from folium.plugins import MarkerCluster
 
@@ -38,6 +40,7 @@ def subset_data(data, f_zipcode, f_attrubutes):
 
     else:
         df = data.copy()
+
     return df
 
 
@@ -81,31 +84,42 @@ st.set_page_config(
 
 
 # Read the data
-data = get_data('data/kc_house_data.csv')
+data_raw = get_data('data/kc_house_data.csv')
 
 # Adding features
-data['price_m2'] = data['price'] / (data['sqft_lot'] * 0.092903)
+data_raw['price_m2'] = data_raw['price'] / (data_raw['sqft_lot'] * 0.092903)
+data_raw['date'] = pd.to_datetime(data_raw['date']).dt.strftime('%Y-%m-%d')
 
-# Showing five lines of data
+#################################
+# Raw sample
+#################################
 st.title("Raw data sample")
-st.write(data.sample(5))
+st.write(data_raw.sample(5, random_state=42))
 
-# Data Overview
+#################################
+# Interactive Data Overview
+#################################
 st.title("Data Overview")
 
+st.sidebar.title('Data Overview Options')
 f_attrubutes = st.sidebar.multiselect(
-    'Enter Columns', data.columns)
+    'Enter Columns', data_raw.columns)
 
 f_zipcode = st.sidebar.multiselect(
-    label='Enter zipcode', options=data['zipcode'].unique())
+    label='Enter zipcode', options=data_raw['zipcode'].unique())
 
 # Subset data
-data = subset_data(
-        data, f_zipcode, f_attrubutes)
-
 st.write(
-    data.sort_values('price')
+    subset_data(
+        data_raw,
+        f_zipcode,
+        f_attrubutes
+    )
 )
+
+#################################
+# Resume average metrics
+#################################
 
 # Create two columns
 c1, c2 = st.columns(2)
@@ -115,76 +129,125 @@ c1.title("Resume averages")
 
 # Calculating metrics
 c1.dataframe(
-    default_metrics(data).sort_values('price_m2'),
+    default_metrics(data_raw).sort_values('price_m2'),
     height=500, width=500)
 
 # Second Column
 c2.title("Statistical descriptive")
 
 # Subset numerical columns
-num_attributes = data.select_dtypes(include=['int64', 'float64'])
+num_attributes = data_raw.select_dtypes(include=['int64', 'float64'])
 num_attributes.drop('id', axis=1, inplace=True)
 
 # Calculating descriptive statistics
-c2.dataframe(describe(num_attributes),
+c2.dataframe(describe(num_attributes).sort_index(axis=0),
              height=500, width=500)
 
 #################################
 # Mapping
 #################################
 
-st.title('Region Overview')
-c1, c2 = st.columns(2)
-c1.header('Portfolio Density')
+# st.title('Region Overview')
+# c1, c2 = st.columns(2)
+# c1.header('Portfolio Density')
 
-df = data.sample(100)
-# Base map
-density_map = folium.Map(
-    location=[data['lat'].mean(), data['long'].mean()],
-    default_zoom_start=15)
-make_cluster = MarkerCluster().add_to(density_map)
+# df = data_raw.sample(50)
+# # Base map
+# density_map = folium.Map(
+#     location=[data_raw['lat'].mean(), data_raw['long'].mean()],
+#     default_zoom_start=15)
+# make_cluster = MarkerCluster().add_to(density_map)
 
-for name, row in df.iterrows():
-    folium.Marker(
-        [row['lat'], row['long']],
-        popup=str(
-            'Price: R$ {:,.2f}\n on {}\n. Features: {}\n sqft, {}\n bedrooms, {}\n bathrooms, {}\n year built'.format(
-                row['price'],
-                row['date'],  # Fix date
-                row['sqft_living'],
-                row['bedrooms'],
-                row['bathrooms'],
-                row['yr_built']
-            ),
-        ),
-        icon=folium.Icon(color='red', prefix='fa', icon='fas fa-home')
-    ).add_to(make_cluster)
+# for name, row in df.iterrows():
+#     folium.Marker(
+#         location=[row['lat'], row['long']],
+#         popup=folium.Popup(
+#             html=folium.IFrame(
+#                 str(
+#                     '<h4><b>Price:</b> R$ {:,.2f}</h4><h4><b>Features:</b></h4><h6>{} sqft</h6><h6>{} bedrooms</h6> <h6>{} bathrooms</h6><h6>{} year built</h6>'.format(
+#                         row['price'],
+#                         row['sqft_living'],
+#                         row['bedrooms'],
+#                         row['bathrooms'],
+#                         row['yr_built']
+#                     )
+#                 )
+#             ),
+#             min_width=200,
+#             max_width=300,
+#             min_height=500,
+#             max_height=1000,
+#         ),
+#         icon=folium.Icon(color='red', prefix='fa', icon='fas fa-home')
+#     ).add_to(make_cluster)
 
-with c1:
-    folium_static(density_map)
+# with c1:
+#     folium_static(density_map)
 
-# Region Price Map
-c2.header('Price density')
-df = data[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
-df.columns = ['ZIP', 'PRICE']
-# df = df.sample(10)
+# # Region Price Map
+# c2.header('Price density')
+# df = data_raw[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
+# df.columns = ['ZIP', 'PRICE']
+# df = df.sample(5)
 
-region_price_map = folium.Map(
-    location=[data['lat'].mean(), data['long'].mean()],
-    default_zoom_start=15)
+# region_price_map = folium.Map(
+#     location=[data_raw['lat'].mean(), data_raw['long'].mean()],
+#     default_zoom_start=15)
 
-geofile = get_geofile()
-geofile = geofile[geofile['ZIP'].isin(df['ZIP'].unique())]
+# geofile = get_geofile()
+# geofile = geofile[geofile['ZIP'].isin(df['ZIP'].unique())]
 
-region_price_map.choropleth(
-    data=df, columns=df.columns,
-    geo_data=geofile,
-    key_on='feature.properties.ZIP',
-    fill_color='YlOrRd', fill_opacity=0.7, line_opacity=0.2,
-    legend_name='Average price')
+# folium.Choropleth(
+#     data=df, columns=df.columns,
+#     geo_data=geofile,
+#     key_on='feature.properties.ZIP',
+#     fill_color='YlOrRd', fill_opacity=0.7, line_opacity=0.2,
+#     legend_name='Average price').add_to(region_price_map)
 
-with c2:
-    folium_static(region_price_map)
+# with c2:
+#     folium_static(region_price_map)
+
+#################################
+# Houses distribution
+#################################
+st.sidebar.title('Commercial Options')
+st.title('Commercial Attributes')
+
+# Filters
+df = data_raw[['yr_built', 'price']].groupby('yr_built').mean().reset_index()
+date_min = data_raw['yr_built'].min()
+date_max = data_raw['yr_built'].max()
+select_range = st.sidebar.select_slider(
+    label='Year built range',
+    options=range(date_min, date_max + 1),
+    value=[date_min, date_max]
+    )
+
+df = df.loc[
+    (df['yr_built'] >= select_range[0]) &
+    (df['yr_built'] <= select_range[1])]
+fig = px.line(df, x='yr_built', y='price')
+st.plotly_chart(fig, use_container_width=True)
+
+# Filters
+df = data_raw[['date', 'price']].groupby('date').mean().reset_index()
+date_min = dt.datetime.strptime(
+    data_raw['date'].min(), '%Y-%m-%d').date()
+date_max = dt.datetime.strptime(
+    data_raw['date'].max(), '%Y-%m-%d').date()
+
+# select_range = st.sidebar.select_slider(
+#     label='Date range',
+#     options=data_raw['date'],
+#     value=[date_min, date_max]
+#     )
+
+# df = df[
+#     (df['date'] >= select_range[0]) &
+#     (df['date'] <= select_range[1])]
+fig = px.line(df, x='date', y='price')
+st.plotly_chart(fig, use_container_width=True)
+
 
 if __name__ == '__main__':
     pass
