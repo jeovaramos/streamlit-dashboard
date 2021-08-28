2aff75cc0318d1ffe32956ced052a3223e25381a
jeovaramos <silvajunior.jeova@gmail.com>
2021-08-28 01:34:24 -0300

    Dashboard sections done

--- lib/sections.py
+++ lib/sections.py
@@ -1,9 +1,11 @@
 from lib.io import ETL
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 import folium
 from streamlit_folium import folium_static
 from folium.plugins import MarkerCluster
+import datetime as dt
 
 
 class Sections():
@@ -58,7 +60,7 @@ class Sections():
         c1, c2 = st.columns(2)
         c1.header('Portfolio Density')
 
-        df = data.sample(50)
+        df = data.sample(1000)
         # Base map
         density_map = folium.Map(
             location=[data['lat'].mean(), data['long'].mean()],
@@ -92,10 +94,11 @@ class Sections():
             folium_static(density_map)
 
         # Region Price Map
-        c2.header('Price density')
+        c2.header('Price average by zipcode')
+
+        df = data.copy()
         df = data[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
         df.columns = ['ZIP', 'PRICE']
-        df = df.sample(5)
 
         region_price_map = folium.Map(
             location=[data['lat'].mean(), data['long'].mean()],
@@ -113,6 +116,137 @@ class Sections():
 
         with c2:
             folium_static(region_price_map)
+
+        return None
+
+    def _slider_max(self, data, feature: str) -> None:
+        slider_filter = st.sidebar.selectbox(
+            f'Max number of {feature}',
+            sorted(
+                set(
+                    data[feature].unique()
+                )
+            ),
+            index=data[feature].unique().shape[0] - 1
+        )
+
+        return slider_filter
+
+    def price_evolution(data: pd.DataFrame) -> None:
+        st.sidebar.title('Commercial Options')
+        st.title('Commercial Attributes')
+
+        # Filters
+        df = data[['yr_built', 'price']].groupby(
+            'yr_built').mean().reset_index()
+        date_min = data['yr_built'].min()
+        date_max = data['yr_built'].max()
+        select_range = st.sidebar.select_slider(
+            label='Year built range',
+            options=range(date_min, date_max + 1),
+            value=[date_min, date_max]
+            )
+
+        df = df.loc[
+            (df['yr_built'] >= select_range[0]) &
+            (df['yr_built'] <= select_range[1])]
+        fig = px.line(df, x='yr_built', y='price')
+        st.plotly_chart(fig, use_container_width=True)
+
+        # Filters
+        df = data[['date', 'price']].groupby('date').mean().reset_index()
+        date_min = ETL.date_format(
+            data['date'].min())
+        date_max = ETL.date_format(
+            data['date'].max())
+        days_interval = range(
+            (date_max + dt.timedelta(days=1) - date_min).days)
+
+        select_range = st.sidebar.select_slider(
+            label='Date range',
+            options=[date_min + dt.timedelta(days=x) for x in days_interval],
+            value=[date_min, date_max]
+            )
+
+        df['date'] = df['date'].apply(ETL.date_format)
+
+        df = df[
+            (df['date'] >= select_range[0]) &
+            (df['date'] <= select_range[1])]
+        fig = px.line(df, x='date', y='price')
+        st.plotly_chart(fig, use_container_width=True)
+
+        return None
+
+    def histogram(data: pd.DataFrame) -> None:
+
+        st.header('Price distribution')
+        st.subheader('Select max price')
+
+        f_price = st.slider(
+            'Price',
+            int(data['price'].min()),
+            int(data['price'].max()),
+            int(data['price'].mean())
+            )
+
+        df = data.loc[data['price'] < f_price]
+        fig = px.histogram(df, x='price', nbins=50)
+        st.plotly_chart(fig, use_container_width=True)
+
+        return None
+
+    def multivariate_analysis(self, data: pd.DataFrame) -> None:
+        st.title('Multivariate Analysis')
+        st.sidebar.title('Multivariate Options')
+
+        options = [
+            'price', 'bathrooms', 'bedrooms', 'floors', 'sqft_living',
+            'sqft_lot', 'sqft_above', 'sqft_basement', 'yr_built',
+            'yr_renovated', 'zipcode'
+            ]
+
+        c1, c2, c3, c4 = st.columns((2, 1, 1, 1))
+        # c3, c4 = st.columns(2)
+
+        # Feature 1
+        c1.header('Feature 1')
+        feature_1 = c1.selectbox('Feature 1', options=options, index=0)
+        filter_1 = self._slider_max(data, feature_1)
+
+        # Feature 2
+        c2.header('Feature 2')
+        feature_2 = c2.selectbox('Feature 2', options=options, index=2)
+        filter_2 = self._slider_max(data, feature_2)
+
+        # Feature 3
+        c3.header('Feature 3')
+        feature_3 = c3.selectbox('Feature 3', options=options, index=3)
+        filter_3 = self._slider_max(data, feature_3)
+
+        # Feature 4
+        c4.header('Feature 4')
+        feature_4 = c4.selectbox('Feature 4', options=options, index=4)
+        filter_4 = self._slider_max(data, feature_4)
+
+        df = data.loc[(data[feature_1] <= filter_1) &
+                      (data[feature_2] <= filter_2) &
+                      (data[feature_3] <= filter_3) &
+                      (data[feature_4] <= filter_4)]
+
+        fig1 = px.histogram(df, x=feature_1, nbins=10)
+        c1.plotly_chart(fig1, use_container_width=True)
+
+        fig2 = px.histogram(df, x=feature_2, nbins=10)
+        c2.plotly_chart(fig2, use_container_width=True)
+
+        fig3 = px.histogram(df, x=feature_3, nbins=10)
+        c3.plotly_chart(fig3, use_container_width=True)
+
+        fig4 = px.histogram(df, x=feature_4, nbins=10)
+        c4.plotly_chart(fig4, use_container_width=True)
+
+        return None
 
 
 if __name__ == '__main__':
