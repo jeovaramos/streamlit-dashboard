9be9b0253ad8a416191183b448ec5bbef8e070d5
jeovaramos <silvajunior.jeova@gmail.com>
2021-08-29 20:15:06 -0300

    Updated the dashboard with the new sections

---
+++ dashboard.py
@@ -0,0 +1,23 @@
+from lib.sections import Sections
+from lib.io import ETL
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="KC Houses - Jeová Ramos",
+    page_icon=":house:",
+    layout="wide")
+
+
+if __name__ == '__main__':
+    Sections().header()
+    Sections.hello_card()
+
+    data = ETL().load_data()
+    ETL().feature_engineering(data)
+    Sections.maps(data)
+    Sections().multivariate_analysis(data)
+    Sections.histogram(data)
+    Sections.price_evolution(data)
+    Sections.resume_metrics(data)
+    Sections.data_overview(data)
